a28becbe4c565a5a49e1f57cbb485c33bb711b63
orventro <orventro@gmail.com>
2023-10-18 21:29:18 +0300

    update grid & add ml agent

---
+++ agents.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm_notebook as tqdm
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+from abc import abstractmethod
+
+class Agent:
+
+    @abstractmethod
+    def predict(self, state):
+        pass
+
+    @abstractmethod
+    def load_history(self, history):
+        pass
+
+class PriceModel:
+
+    def __init__(self, model: SARIMAX | None = None):
+        pass
+    
+    def fit(self, df: pd.DataFrame):
+        self.history = df
+        self.model = SARIMAX(df, order=(5, 1, 1))
+        self.model = self.model.fit(full_output=False, disp=False)
+    
+    def predict(self, current_time: datetime, current_price: float, n: int):
+        self.history.loc[current_time] = current_price
+        new_model = SARIMAX(self.history, order=(5, 1, 1))
+        new_model = new_model.filter(self.model.params)
+        return new_model.forecast(n).values
+
+class MLAgent(Agent):
+
+    def __init__(self, price_model: PriceModel | None =None):
+        if price_model is None:
+            self.price_model = PriceModel()
+        else:
+            self.price_model = price_model
+        
+        self.timestep = 0
+        self.dt = 5/60
+        self.hydro_cost = 0.06
+        self.hydro_prod = 100
+        self.hydro_penalty = 10
+        self.hydro_amount = 1000
+        self.ts_start = 0
+
+        self.num_samples = 100
+        self.max_price_quantile = 0.3
+    
+    def load_history(self, history):
+        self.history = history.copy()
+        # self.price_model.fit(self.history, "20220601")
+    
+    def reset(self, info: dict):
+        self.ts_start = info['ts_start']
+        self.price_model.fit(self.history.iloc[:self.ts_start])
+
+    def predict(self, state):
+        t, batt, price, sol, hydro = state
+        ts_to_buy = int(np.ceil((1-hydro) * self.hydro_amount / self.hydro_prod / self.dt))
+        ts_left = int(np.round(288 * (1 - t)))
+        current_ts_global = self.ts_start+self.timestep
+        self.timestep += 1
+        if ts_left <= 1:
+            return np.array([0, 0])
+
+        row = self.history.iloc[current_ts_global]
+        current_time, current_price = row.name, row.MW
+        pred = self.price_model.predict(current_time, current_price, ts_left)
+        pred = np.squeeze(pred)
+        max_price1 = np.sort(pred)[min(ts_to_buy, ts_left-1)] 
+        max_price2 = np.sort(self.history[current_ts_global:current_ts_global+ts_left])[min(ts_to_buy, ts_left-1)] # the real price, to compare
+        action = np.array([0, 0])
+        if max_price1 >= current_price or ts_left <= ts_to_buy:
+            action[0] = 1
+        if current_price < 0:
+            action[1] = 1
+        # print(max_price1, max_price2, price, current_price, action, hydro, '_', ts_to_buy, ts_left)
+        return action
--- grid.py
+++ grid.py
@@ -12,7 +12,8 @@ class Microgrid(Env):
         self.action_space = Box(low=np.array([0, 0]), high=np.array([1, 1]))
         self.observation_space = Box(low=np.array([0, 0, -1, 0, 0]), high=np.array([1, 1, 10, 2, 1]))
         self.state = np.array([0, 0.2, 1, 0, 0])
-        self.sim_length = 288
+        self.sim_length = 2880
+        self.day_length = 288
 
         # temp hardcoded values
         self.timestep = 0
@@ -28,21 +29,27 @@ class Microgrid(Env):
         self.hydro_penalty = 10
         self.hydro_amount = 1000
         
-    def step(self, action: Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+    def step(self, action: tuple[float, float]) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+        power_price = self.power_price[self.ts_start + self.timestep]
+        solar_power = self.solar_power[self.ts_start + self.timestep]
+
         # don't procude hydro after quota is reached
-        action[0] = min(action[0], (1 - self.state[4]) * self.hydro_amount / self.hydro_prod)
+        action[0] = min(action[0], (1 - self.state[4]) * self.hydro_amount / self.hydro_prod / self.dt)
         batt_pwr = action[1] * 2 - 1
         # don't take energy from empty battery
         batt_pwr = min(self.state[1] / self.dt, batt_pwr)
         # don't put energy into a full battery
         batt_pwr = max(batt_pwr, (self.state[1] - 1) / self.dt)
         action_pwr = action * [self.hydro_prod*self.hydro_cost, self.batt_pwr]
-        grid_load = (action_pwr[0] - action_pwr[1] - self.solar_power[self.timestep]) * self.dt
-        reward = self.power_price[self.timestep] * grid_load / self.price_norm
-        self.state[0] += 1/self.sim_length
+        grid_load = (action_pwr[0] - action_pwr[1] - solar_power) * self.dt
+        reward = power_price * grid_load / self.price_norm
+        self.state[0] += 1/self.day_length
+        if self.state[0] >= 1:
+            self.state[0] -= 1
+            self.state[4] = 0
         self.state[1] -= batt_pwr*self.dt
-        self.state[2] = min(10, self.power_price[self.timestep] / self.price_norm)
-        self.state[3] = min(2, self.solar_power[self.timestep] / self.solar_norm)
+        self.state[2] = min(10, power_price / self.price_norm)
+        self.state[3] = min(2, solar_power / self.solar_norm)
         self.state[4] += action[0] * self.hydro_prod / self.hydro_amount * self.dt
         self.timestep += 1
         done = self.timestep == self.sim_length
@@ -60,8 +67,8 @@ class Microgrid(Env):
         if data is not None:
             self.data = data
         rng = random.Random(seed)
-        day = rng.choice(self.data)
-        self.solar_power = day['power']
-        self.power_price = day['price']
-        info = {}
+        self.ts_start = rng.randint(10000, len(self.data)-30000)
+        self.power_price = self.data['price']
+        self.solar_power = self.data['power']
+        info = {'ts_start': self.ts_start}
         return self.state, info
